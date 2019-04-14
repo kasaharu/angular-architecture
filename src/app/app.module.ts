aeac2d17c767f9c0f5cd3d986b266f80b5191a03
Wataru.Kasahara <kasahara1019@gmail.com>
2019-04-14 20:51:27 +0900

    yarn ng g c components/user-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './components/user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
