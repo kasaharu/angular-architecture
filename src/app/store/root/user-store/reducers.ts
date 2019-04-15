123dd32252e7f65f995adf3092abbfab8e30186e
Wataru.Kasahara <kasahara1019@gmail.com>
2019-04-15 21:58:07 +0900

    UserStore の Reducer を実装

---
+++ src/app/store/root/user-store/reducers.ts
@@ -0,0 +1,19 @@
+import * as Actions from './actions';
+import { initialState, State } from './state';
+
+export function userReducer(state: State = initialState, action: Actions.ActionsUnion) {
+  switch (action.type) {
+    case Actions.saveRequest.type: {
+      return state;
+    }
+    case Actions.saveSuccess.type: {
+      return {
+        ...state,
+        user: action.payload,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+}
