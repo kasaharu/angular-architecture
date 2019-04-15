788309c3f311c5d6d08208241e24b77b8cba2827
Wataru.Kasahara <kasahara1019@gmail.com>
2019-04-15 22:06:06 +0900

    RootStoreModule で userReducer を import

--- src/app/store/root-store.module.ts
+++ src/app/store/root-store.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+
+import { userReducer } from './root/user-store/reducers';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, StoreModule.forRoot({ user: userReducer })],
 })
-export class RootStoreModule { }
+export class RootStoreModule {}
