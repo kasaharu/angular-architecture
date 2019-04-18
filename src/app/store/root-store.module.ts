b3fd95f34e7315f6f28b6b32ffe0e4853c9a1e26
Wataru.Kasahara <kasahara1019@gmail.com>
2019-04-18 22:03:18 +0900

    RootStoreModule で UserEffects を import

--- src/app/store/root-store.module.ts
+++ src/app/store/root-store.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import { userReducer } from './root/user-store/reducers';
+import { UserEffects } from './root/user-store/effects';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, StoreModule.forRoot({ user: userReducer })],
+  imports: [CommonModule, StoreModule.forRoot({ user: userReducer }), EffectsModule.forRoot([UserEffects])],
 })
 export class RootStoreModule {}
