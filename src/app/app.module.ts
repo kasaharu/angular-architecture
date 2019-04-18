5543d7d05763acd8efa8e21001da15d29fcd6af7
Wataru.Kasahara <kasahara1019@gmail.com>
2019-04-18 21:28:28 +0900

    StoreDevtooslModule を AppModule で import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './components/user-list/user-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    UserListComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, UserListComponent],
+  imports: [BrowserModule, AppRoutingModule, StoreDevtoolsModule.instrument({ maxAge: 25 })],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
