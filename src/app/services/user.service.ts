d567c2b536becbcb64aaa698ecb30530a81406c3
Wataru.Kasahara <kasahara1019@gmail.com>
2019-04-20 14:23:58 +0900

    Layered Architecture を意識した Repository 層の分割

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+
+import { UserRepository } from '../repositories/user.repository';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private http: HttpClient) {}
+  constructor(private userRepository: UserRepository) {}
 
   fetchUserList() {
-    return this.http.get<any>('https://jsonplaceholder.typicode.com/users');
+    return this.userRepository.fetchUserList();
   }
 }
