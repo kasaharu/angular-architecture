c2175b7a300c2ada11b07c2876ec1b54bc85f092
Wataru.Kasahara <kasahara1019@gmail.com>
2019-04-20 15:26:55 +0900

    不要になった UserStore の Effects を削除

--- src/app/store/root-store.module.ts
+++ src/app/store/root-store.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
 
 import { userReducer } from './root/user-store/reducers';
-import { UserEffects } from './root/user-store/effects';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, StoreModule.forRoot({ user: userReducer }), EffectsModule.forRoot([UserEffects])],
+  imports: [CommonModule, StoreModule.forRoot({ user: userReducer })],
 })
 export class RootStoreModule {}
