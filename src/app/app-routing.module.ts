3f8f57062fc2ac897220faca3489e52d03124bae
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-12 17:25:19 +0900

    / にアクセスしたときに表示する component を HomeComponent に変更

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UserListComponent } from './components/user-list/user-list.component';
 
-const routes: Routes = [{ path: '', component: UserListComponent }];
+import { HomeComponent } from './containers/home/home.component';
+
+const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
