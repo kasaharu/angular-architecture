283e9d4df32fef1e0a8472ff7e35ef788af5d6a4
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-12 16:41:53 +0900

    UserUsecase が依存している Repository と Store も mock を provide

--- src/app/usecases/user.usecase.spec.ts
+++ src/app/usecases/user.usecase.spec.ts
@@ -1,12 +1,29 @@
 import { TestBed } from '@angular/core/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 
+import { UserRepository } from '../repositories/user.repository';
 import { UserUsecase } from './user.usecase';
+
+class MockUserRepository {
+  fetchUserList() {}
+}
 
 describe('UserUsecase', () => {
   let usecase: UserUsecase;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: UserRepository, useClass: MockUserRepository },
+        provideMockStore({
+          initialState: {
+            user: {
+              userList: null,
+            },
+          },
+        }),
+      ],
+    });
 
     usecase = TestBed.get(UserUsecase);
   });
