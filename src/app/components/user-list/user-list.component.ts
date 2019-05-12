4842cde08fd46a1fcd284e07414291ad50ab198b
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-12 17:43:14 +0900

    UserListComponent で表示する user 一覧を Container から Input で受け取る

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { User } from '../../core/models';
 
 @Component({
   selector: 'app-user-list',
@@ -6,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-list.component.scss'],
 })
 export class UserListComponent implements OnInit {
+  @Input()
+  userList: User[];
+
   constructor() {}
 
   ngOnInit() {}
