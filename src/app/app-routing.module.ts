52c7def9cae43165f3446f800c06e55ef28ddf57
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-14 09:55:21 +0900

    user 詳細の URL を変更

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { DetailComponent } from './containers/detail/detail.component';
 import { HomeComponent } from './containers/home/home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }, { path: 'detail/:user_id', component: DetailComponent }];
+const routes: Routes = [{ path: '', component: HomeComponent }, { path: 'users/:id', component: DetailComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
