1576c556532b4d495c18a867f5f8071c4d6f488e
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-16 09:47:09 +0900

    UserStoreState で選択中の user を管理するための selectedUser プロパティを追加

--- src/app/store/root/user-store/state.ts
+++ src/app/store/root/user-store/state.ts
@@ -3,14 +3,20 @@ import { User } from '../../../core/models';
 
 export interface State {
   userList: User[] | null;
+  selectedUser: User | null;
 }
 
 export const initialState: State = {
   userList: null,
+  selectedUser: null,
 };
 
 export const selectUserFeature = createFeatureSelector<State>('user');
 export const selectUserList = createSelector(
   selectUserFeature,
   (state) => state.userList,
+);
+export const selectSelectedUser = createSelector(
+  selectUserFeature,
+  (state) => state.selectedUser,
 );
