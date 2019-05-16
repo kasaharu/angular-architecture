160818a67de7a07696ad4e93e9029e9972a69cac
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-16 09:54:59 +0900

    UserStore の reducer の返り値を指定

--- src/app/store/root/user-store/reducers.ts
+++ src/app/store/root/user-store/reducers.ts
@@ -1,7 +1,7 @@
 import * as Actions from './actions';
 import { initialState, State } from './state';
 
-export function userReducer(state: State = initialState, action: Actions.ActionsUnion) {
+export function userReducer(state: State = initialState, action: Actions.ActionsUnion): State {
   switch (action.type) {
     case Actions.saveUserList.type: {
       return {
