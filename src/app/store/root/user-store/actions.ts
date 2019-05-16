70bf429811f1f1453bbd8019ea1b5eeba659efec
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-16 09:51:49 +0900

    選択中の user を保持するための action を定義

--- src/app/store/root/user-store/actions.ts
+++ src/app/store/root/user-store/actions.ts
@@ -2,6 +2,8 @@ import { createAction, union } from '@ngrx/store';
 import { User } from '../../../core/models';
 
 export const saveUserList = createAction('[User] save user list', (payload: User[]) => ({ payload }));
-const actions = union({ saveUserList: saveUserList });
+export const saveSelectedUser = createAction('[User] save selected user', (payload: User) => ({ payload }));
+
+const actions = union({ saveUserList, saveSelectedUser });
 
 export type ActionsUnion = typeof actions;
