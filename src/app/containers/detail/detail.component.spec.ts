2bfe5a6dbb0582f1ab96c172ba1b473c949cfbd9
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-17 10:12:11 +0900

    DetailComponentSpec で ActivatedRoute の依存を解決

--- src/app/containers/detail/detail.component.spec.ts
+++ src/app/containers/detail/detail.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { DetailComponent } from './detail.component';
 
@@ -9,6 +10,7 @@ describe('DetailComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [DetailComponent],
+      imports: [RouterTestingModule],
     }).compileComponents();
   }));
 
