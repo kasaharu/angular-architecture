bf67407c350900ca0a59c32eda5dbee6a60b1293
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-17 10:16:49 +0900

    DetailComponentSpec 依存している query, usecase の mock class を作成

--- src/app/containers/detail/detail.component.spec.ts
+++ src/app/containers/detail/detail.component.spec.ts
@@ -1,7 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { BehaviorSubject } from 'rxjs';
 
+import { User } from '../../core/models';
+import { UserQuery } from '../../queries/user.query';
+import { DetailUsecase } from '../../usecases/detail.usecase';
 import { DetailComponent } from './detail.component';
+
+class MockUserQuery {
+  selectedUser$ = new BehaviorSubject<User | null>(null);
+}
+
+class MockDetailUsecase {
+  initialize() {}
+}
 
 describe('DetailComponent', () => {
   let component: DetailComponent;
@@ -11,6 +23,7 @@ describe('DetailComponent', () => {
     TestBed.configureTestingModule({
       declarations: [DetailComponent],
       imports: [RouterTestingModule],
+      providers: [{ provide: UserQuery, useClass: MockUserQuery }, { provide: DetailUsecase, useClass: MockDetailUsecase }],
     }).compileComponents();
   }));
 
