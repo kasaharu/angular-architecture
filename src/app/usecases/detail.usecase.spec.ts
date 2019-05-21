eef8767a45df9bd06870a52f89fc8722f39f5a54
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-21 10:11:31 +0900

    DetailUsecase が依存している Store の mock を provide

--- src/app/usecases/detail.usecase.spec.ts
+++ src/app/usecases/detail.usecase.spec.ts
@@ -1,9 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DetailUsecase } from './detail.usecase';
+import { provideMockStore } from '@ngrx/store/testing';
 
 describe('DetailUsecase', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        provideMockStore({
+          initialState: {
+            user: {
+              selectedUser: null,
+            },
+          },
+        }),
+      ],
+    });
+  });
 
   it('should be created', () => {
     const usecase: DetailUsecase = TestBed.get(DetailUsecase);
