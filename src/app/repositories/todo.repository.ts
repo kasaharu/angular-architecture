71c27d9cefa571de6fe4e1a93638b1328e3470fa
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-22 20:37:58 +0900

    該当 user の todo 一覧を取得する API メソッドを repository に実装

--- src/app/repositories/todo.repository.ts
+++ src/app/repositories/todo.repository.ts
@@ -1,8 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Todo } from '../core/models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoRepository {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  fetchTodoListBy(userId: number): Observable<Todo[]> {
+    return this.http.get<Todo[]>(`https://jsonplaceholder.typicode.com/todos?userId=${userId}`);
+  }
 }
