8aa28399710247f7afd2b7265978f047c7b6d534
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-24 09:58:09 +0900

    TodoStore を RootStoreModule で import

--- src/app/store/root-store.module.ts
+++ src/app/store/root-store.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
 
 import { userReducer } from './root/user-store/reducers';
+import { todoReducer } from './root/todo-store/reducers';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, StoreModule.forRoot({ user: userReducer })],
+  imports: [CommonModule, StoreModule.forRoot({ user: userReducer, todo: todoReducer })],
 })
 export class RootStoreModule {}
