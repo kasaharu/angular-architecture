aa10563cae02f0d54b4d3d2cd0b3e9520f660e28
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-24 10:05:28 +0900

    user 詳細の initialize 処理で todo 一覧を取得

--- src/app/usecases/detail.usecase.ts
+++ src/app/usecases/detail.usecase.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 
+import { TodoRepository } from '../repositories/todo.repository';
 import { UserRepository } from '../repositories/user.repository';
+import { TodoStoreActions } from '../store/root/todo-store';
 import { UserStoreActions } from '../store/root/user-store';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DetailUsecase {
-  constructor(private store$: Store<{}>, private userRepository: UserRepository) {}
+  constructor(private store$: Store<{}>, private userRepository: UserRepository, private todoRepository: TodoRepository) {}
 
   async initialize(userId: number) {
     const user = await this.userRepository.fetchUser(userId).toPromise();
     this.store$.dispatch(UserStoreActions.saveSelectedUser(user));
+    const todoList = await this.todoRepository.fetchTodoListBy(user.id).toPromise();
+    this.store$.dispatch(TodoStoreActions.saveTodoList(todoList));
   }
 }
