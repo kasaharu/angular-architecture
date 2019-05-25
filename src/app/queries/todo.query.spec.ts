f2a90b9cef54c8f6e80ac099d69f9a53e91c2c91
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-25 23:01:26 +0900

    todo 一覧を store から取得するための query を用意

--- src/app/queries/todo.query.spec.ts
+++ src/app/queries/todo.query.spec.ts
@@ -1,9 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TodoQuery } from './todo.query';
+import { provideMockStore } from '@ngrx/store/testing';
 
 describe('TodoQuery', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        provideMockStore({
+          initialState: {
+            todo: {
+              todoList: null,
+            },
+          },
+        }),
+      ],
+    });
+  });
 
   it('should be created', () => {
     const service: TodoQuery = TestBed.get(TodoQuery);
--- src/app/queries/todo.query.ts
+++ src/app/queries/todo.query.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { Todo } from '../core/models';
+import { TodoStoreState } from '../store/root/todo-store';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoQuery {
-  constructor() {}
+  constructor(private store$: Store<{}>) {}
+
+  todoList$: Observable<Todo[]> = this.store$.pipe(select(TodoStoreState.selectTodoList));
 }
