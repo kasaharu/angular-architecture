40a8785df18b8a556ed2745a0891596d93f35e84
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-26 17:06:54 +0900

    DetailComponentSpec 依存している TodoQuery の mock class を作成

--- src/app/containers/detail/detail.component.spec.ts
+++ src/app/containers/detail/detail.component.spec.ts
@@ -2,13 +2,18 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { BehaviorSubject } from 'rxjs';
 
-import { User } from '../../core/models';
+import { Todo, User } from '../../core/models';
+import { TodoQuery } from '../../queries/todo.query';
 import { UserQuery } from '../../queries/user.query';
 import { DetailUsecase } from '../../usecases/detail.usecase';
 import { DetailComponent } from './detail.component';
 
 class MockUserQuery {
   selectedUser$ = new BehaviorSubject<User | null>(null);
+}
+
+class MockTodoQuery {
+  todoList$ = new BehaviorSubject<Todo[] | null>(null);
 }
 
 class MockDetailUsecase {
@@ -23,7 +28,11 @@ describe('DetailComponent', () => {
     TestBed.configureTestingModule({
       declarations: [DetailComponent],
       imports: [RouterTestingModule],
-      providers: [{ provide: UserQuery, useClass: MockUserQuery }, { provide: DetailUsecase, useClass: MockDetailUsecase }],
+      providers: [
+        { provide: UserQuery, useClass: MockUserQuery },
+        { provide: TodoQuery, useClass: MockTodoQuery },
+        { provide: DetailUsecase, useClass: MockDetailUsecase },
+      ],
     }).compileComponents();
   }));
 
