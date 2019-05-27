8e31f0beb39b0a67e3c4144cc9449f23a94f0348
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-28 00:09:06 +0900

    詳細ページに child routing を設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AlbumComponent } from './containers/album/album.component';
 import { DetailComponent } from './containers/detail/detail.component';
 import { HomeComponent } from './containers/home/home.component';
+import { PostComponent } from './containers/post/post.component';
+import { TodoComponent } from './containers/todo/todo.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }, { path: 'users/:id', component: DetailComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'users/:id',
+    component: DetailComponent,
+    children: [
+      { path: 'todos', component: TodoComponent },
+      { path: 'posts', component: PostComponent },
+      { path: 'albums', component: AlbumComponent },
+      { path: '', redirectTo: 'todos', pathMatch: 'full' },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
