8698f18cfcfcaf9d8ce9dcfff7027096b9e8b6dc
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-27 23:56:05 +0900

    yarn ng g c containers/todo --lint-fix true

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,10 @@ import { DetailComponent } from './containers/detail/detail.component';
 import { TodoListComponent } from './presenters/todo-list/todo-list.component';
 import { PostListComponent } from './presenters/post-list/post-list.component';
 import { AlbumListComponent } from './presenters/album-list/album-list.component';
+import { TodoComponent } from './containers/todo/todo.component';
 
 @NgModule({
-  declarations: [AppComponent, UserListComponent, HomeComponent, DetailComponent, TodoListComponent, PostListComponent, AlbumListComponent],
+  declarations: [AppComponent, UserListComponent, HomeComponent, DetailComponent, TodoListComponent, PostListComponent, AlbumListComponent, TodoComponent],
   imports: [BrowserModule, AppRoutingModule, StoreDevtoolsModule.instrument({ maxAge: 25 }), HttpClientModule, RootStoreModule],
   providers: [],
   bootstrap: [AppComponent],
