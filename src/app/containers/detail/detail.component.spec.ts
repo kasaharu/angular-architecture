7eda207841337744e559aeb021a99c253f45cf24
Wataru.Kasahara <kasahara1019@gmail.com>
2019-05-27 19:53:12 +0900

    todo 一覧表示部分を子 component 化

--- src/app/containers/detail/detail.component.spec.ts
+++ src/app/containers/detail/detail.component.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { BehaviorSubject } from 'rxjs';
@@ -33,6 +34,7 @@ describe('DetailComponent', () => {
         { provide: TodoQuery, useClass: MockTodoQuery },
         { provide: DetailUsecase, useClass: MockDetailUsecase },
       ],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
   }));
 
--- src/app/presenters/todo-list/todo-list.component.ts
+++ src/app/presenters/todo-list/todo-list.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Todo } from 'src/app/core/models';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
+  @Input()
+  todoList: Todo[];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
