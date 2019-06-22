b1041e3b854d28b5a6e8d3c51c72861859095fe5
Wataru.Kasahara <kasahara1019@gmail.com>
2019-06-22 15:41:08 +0900

    該当 user の post 一覧を取得する API メソッドを repository に実装

--- src/app/repositories/post.repository.ts
+++ src/app/repositories/post.repository.ts
@@ -1,8 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from '../core/models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PostRepository {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  fetchPostListBy(userId: number): Observable<Post[]> {
+    return this.http.get<Post[]>(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
+  }
 }
