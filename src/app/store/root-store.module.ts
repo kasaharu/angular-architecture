58fcea13442d8011396130b9412f066ce4f24cd0
Wataru.Kasahara <kasahara1019@gmail.com>
2019-06-22 15:11:09 +0900

    AlbumStore を StoreModule に登録

--- src/app/store/root-store.module.ts
+++ src/app/store/root-store.module.ts
@@ -5,9 +5,10 @@ import { StoreModule } from '@ngrx/store';
 import { userReducer } from './root/user-store/reducers';
 import { todoReducer } from './root/todo-store/reducers';
 import { postReducer } from './root/post-store/reducers';
+import { albumReducer } from './root/album-store/reducers';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, StoreModule.forRoot({ user: userReducer, todo: todoReducer, post: postReducer })],
+  imports: [CommonModule, StoreModule.forRoot({ user: userReducer, todo: todoReducer, post: postReducer, album: albumReducer })],
 })
 export class RootStoreModule {}
