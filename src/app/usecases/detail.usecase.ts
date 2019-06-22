749a4c4751229e64b5f8b95bce64853f8d5aa56f
Wataru.Kasahara <kasahara1019@gmail.com>
2019-06-22 15:45:54 +0900

    詳細ページ表示時に post 一覧を取得

--- src/app/usecases/detail.usecase.ts
+++ src/app/usecases/detail.usecase.ts
@@ -5,17 +5,26 @@ import { TodoRepository } from '../repositories/todo.repository';
 import { UserRepository } from '../repositories/user.repository';
 import { TodoStoreActions } from '../store/root/todo-store';
 import { UserStoreActions } from '../store/root/user-store';
+import { PostRepository } from '../repositories/post.repository';
+import { PostStoreActions } from '../store/root/post-store';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DetailUsecase {
-  constructor(private store$: Store<{}>, private userRepository: UserRepository, private todoRepository: TodoRepository) {}
+  constructor(
+    private store$: Store<{}>,
+    private userRepository: UserRepository,
+    private todoRepository: TodoRepository,
+    private postRepository: PostRepository,
+  ) {}
 
   async initialize(userId: number) {
     const user = await this.userRepository.fetchUser(userId).toPromise();
     this.store$.dispatch(UserStoreActions.saveSelectedUser(user));
     const todoList = await this.todoRepository.fetchTodoListBy(user.id).toPromise();
     this.store$.dispatch(TodoStoreActions.saveTodoList(todoList));
+    const postList = await this.postRepository.fetchPostListBy(user.id).toPromise();
+    this.store$.dispatch(PostStoreActions.savePostList(postList));
   }
 }
