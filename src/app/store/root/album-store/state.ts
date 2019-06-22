d45cf6dec4ffff60ea2be62c5bccca28c04b8b9e
Wataru.Kasahara <kasahara1019@gmail.com>
2019-06-22 15:03:24 +0900

    AlbumStore の Selector を定義

--- src/app/store/root/album-store/state.ts
+++ src/app/store/root/album-store/state.ts
@@ -1,3 +1,4 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { Album } from '../../../core/models';
 
 export interface State {
@@ -7,3 +8,10 @@ export interface State {
 export const initialState: State = {
   albumList: null,
 };
+
+export const selectTodoFeature = createFeatureSelector<State>('album');
+
+export const selectTodoList = createSelector(
+  selectTodoFeature,
+  (state) => state.albumList,
+);
