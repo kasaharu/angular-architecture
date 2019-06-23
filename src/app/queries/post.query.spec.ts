59460f8113e3b1bd4f95ff8004a0567effa8370f
Wataru.Kasahara <kasahara1019@gmail.com>
2019-06-23 21:04:05 +0900

    post 一覧を store から取得するための query を実装

--- src/app/queries/post.query.spec.ts
+++ src/app/queries/post.query.spec.ts
@@ -1,9 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PostQuery } from './post.query';
+import { provideMockStore } from '@ngrx/store/testing';
 
 describe('PostQuery', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        provideMockStore({
+          initialState: {
+            post: {
+              postList: null,
+            },
+          },
+        }),
+      ],
+    });
+  });
 
   it('should be created', () => {
     const query: PostQuery = TestBed.get(PostQuery);
--- src/app/queries/post.query.ts
+++ src/app/queries/post.query.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Post } from '../core/models';
+import { PostStoreState } from '../store/root/post-store';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostQuery {
+  constructor(private store$: Store<{}>) { }
 
-  constructor() { }
+  postList$: Observable<Post[]> = this.store$.pipe(select(PostStoreState.selectTodoList));
 }
