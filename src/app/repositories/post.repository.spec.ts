3b5fcf179b8001ddb5124c9b81cf7cf6cf330827
Wataru.Kasahara <kasahara1019@gmail.com>
2019-06-29 17:34:25 +0900

    PostRepository の spec 追加

--- src/app/repositories/post.repository.spec.ts
+++ src/app/repositories/post.repository.spec.ts
@@ -1,12 +1,31 @@
+import { HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
+import { Post } from '../core/models';
 import { PostRepository } from './post.repository';
 
 describe('PostRepository', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let repository: PostRepository;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+
+    repository = TestBed.get(PostRepository);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
 
   it('should be created', () => {
-    const repository: PostRepository = TestBed.get(PostRepository);
     expect(repository).toBeTruthy();
+  });
+
+  it('call fetchPostListBy() method', () => {
+    const payload: Post[] = [];
+    const userId = 1;
+    repository.fetchPostListBy(userId).subscribe((result) => expect(result).toEqual(payload));
+
+    const req = httpTestingController.expectOne(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
+    expect(req.request.method).toEqual('GET');
+    req.flush(payload);
+    httpTestingController.verify();
   });
 });
