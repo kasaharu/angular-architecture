588e9353e6b92d5157fb412d459946c15ffc95d9
Wataru.Kasahara <kasahara1019@gmail.com>
2019-06-29 17:17:38 +0900

    UserRepository の spec 追加

--- src/app/repositories/user.repository.spec.ts
+++ src/app/repositories/user.repository.spec.ts
@@ -1,17 +1,50 @@
+import { HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
+import { User } from '../core/models';
 import { UserRepository } from './user.repository';
 
 describe('UserRepository', () => {
   let repository: UserRepository;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
 
     repository = TestBed.get(UserRepository);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
   it('should create an instance', () => {
     expect(repository).toBeTruthy();
+  });
+
+  it('call fetchUserList() method', () => {
+    const payload: User[] = [];
+    repository.fetchUserList().subscribe((result) => expect(result).toEqual(payload));
+
+    const req = httpTestingController.expectOne('https://jsonplaceholder.typicode.com/users');
+    expect(req.request.method).toEqual('GET');
+    req.flush(payload);
+    httpTestingController.verify();
+  });
+
+  it('call fetchUser() method', () => {
+    const payload: User = {
+      id: 1,
+      name: '',
+      username: '',
+      email: '',
+      address: { street: '', suite: '', city: '', zipcode: '', geo: { lat: 0, lng: 0 } },
+      phone: '',
+      website: '',
+      company: { name: '', catchPhrase: '', bs: '' },
+    };
+    const userId = 1;
+    repository.fetchUser(userId).subscribe((result) => expect(result).toEqual(payload));
+
+    const req = httpTestingController.expectOne(`https://jsonplaceholder.typicode.com/users/${userId}`);
+    expect(req.request.method).toEqual('GET');
+    req.flush(payload);
+    httpTestingController.verify();
   });
 });
