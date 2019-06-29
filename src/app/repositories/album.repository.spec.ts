54983e94e4f9302b297938e086bb33f5f216e71e
Wataru.Kasahara <kasahara1019@gmail.com>
2019-06-29 17:36:43 +0900

    AlbumRepository の spec 追加

--- src/app/repositories/album.repository.spec.ts
+++ src/app/repositories/album.repository.spec.ts
@@ -1,12 +1,31 @@
+import { HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
 import { AlbumRepository } from './album.repository';
+import { Album } from '../core/models';
 
 describe('AlbumRepository', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let repository: AlbumRepository;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+
+    repository = TestBed.get(AlbumRepository);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
 
   it('should be created', () => {
-    const repository: AlbumRepository = TestBed.get(AlbumRepository);
     expect(repository).toBeTruthy();
+  });
+
+  it('call fetchAlbumListBy() method', () => {
+    const payload: Album[] = [];
+    const userId = 1;
+    repository.fetchAlbumListBy(userId).subscribe((result) => expect(result).toEqual(payload));
+
+    const req = httpTestingController.expectOne(`https://jsonplaceholder.typicode.com/albums?userId=${userId}`);
+    expect(req.request.method).toEqual('GET');
+    req.flush(payload);
+    httpTestingController.verify();
   });
 });
