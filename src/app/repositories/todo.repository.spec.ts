69a7980454784177f44e6e23d86aa79a38be1951
Wataru.Kasahara <kasahara1019@gmail.com>
2019-06-29 17:25:45 +0900

    TodoRepository の spec 追加

--- src/app/repositories/todo.repository.spec.ts
+++ src/app/repositories/todo.repository.spec.ts
@@ -1,12 +1,31 @@
+import { HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
+import { Todo } from '../core/models';
 import { TodoRepository } from './todo.repository';
 
 describe('TodoRepository', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let repository: TodoRepository;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+
+    repository = TestBed.get(TodoRepository);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
 
   it('should be created', () => {
-    const service: TodoRepository = TestBed.get(TodoRepository);
-    expect(service).toBeTruthy();
+    expect(repository).toBeTruthy();
+  });
+
+  it('call fetchTodoListBy() method', () => {
+    const payload: Todo[] = [];
+    const userId = 1;
+    repository.fetchTodoListBy(userId).subscribe((result) => expect(result).toEqual(payload));
+
+    const req = httpTestingController.expectOne(`https://jsonplaceholder.typicode.com/todos?userId=${userId}`);
+    expect(req.request.method).toEqual('GET');
+    req.flush(payload);
+    httpTestingController.verify();
   });
 });
