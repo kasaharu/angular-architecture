fe229954350e305ec61f0383b0485223655bbad2
Wataru.Kasahara <kasahara1019@gmail.com>
2019-07-02 07:56:12 +0900

    UserUsecase の spec 追加

--- src/app/usecases/user.usecase.spec.ts
+++ src/app/usecases/user.usecase.spec.ts
@@ -1,7 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-import { provideMockStore } from '@ngrx/store/testing';
-
+import { Store } from '@ngrx/store';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { of } from 'rxjs';
+import { skip } from 'rxjs/operators';
+import { User } from '../core/models';
 import { UserRepository } from '../repositories/user.repository';
+import { UserStoreActions } from '../store/root/user-store';
 import { UserUsecase } from './user.usecase';
 
 class MockUserRepository {
@@ -9,7 +13,9 @@ class MockUserRepository {
 }
 
 describe('UserUsecase', () => {
+  let repository: MockUserRepository;
   let usecase: UserUsecase;
+  let store$: MockStore<{}>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -25,9 +31,37 @@ describe('UserUsecase', () => {
       ],
     });
 
+    repository = TestBed.get(UserRepository);
     usecase = TestBed.get(UserUsecase);
+    store$ = TestBed.get(Store);
+    spyOn(store$, 'dispatch').and.callThrough();
   });
+
   it('should create an instance', () => {
     expect(usecase).toBeTruthy();
+  });
+
+  it('call initialize() method', async () => {
+    const userList: User[] = [
+      {
+        id: 1,
+        name: '',
+        username: '',
+        email: '',
+        address: { street: '', suite: '', city: '', zipcode: '', geo: { lat: 0, lng: 0 } },
+        phone: '',
+        website: '',
+        company: { name: '', catchPhrase: '', bs: '' },
+      },
+    ];
+    const saveAction = UserStoreActions.saveUserList(userList);
+    const calledActionList = [saveAction];
+
+    spyOn(repository, 'fetchUserList').and.returnValue(of(userList));
+    const actions: any = [];
+    store$.scannedActions$.pipe(skip(1)).subscribe((action) => actions.push(action));
+    await usecase.initialize();
+
+    expect(actions).toEqual(calledActionList);
   });
 });
