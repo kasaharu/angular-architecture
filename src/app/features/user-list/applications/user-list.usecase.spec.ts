05f374ae6907100a8a7fec45a045e29063da1d6a
Wataru.Kasahara <kasahara1019@gmail.com>
2020-01-13 13:08:51 +0900

    initialize 処理で userList を取得し store に保存

--- src/app/features/user-list/applications/user-list.usecase.spec.ts
+++ src/app/features/user-list/applications/user-list.usecase.spec.ts
@@ -1,12 +1,55 @@
 import { TestBed } from '@angular/core/testing';
-
+import { Action, Store } from '@ngrx/store';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { UserRepository } from '../../../infrastructures/repositories/user.repository';
+import { User } from '../domain/user-list';
+import { actions as userListActions } from './user-list.store';
 import { UserListUsecase } from './user-list.usecase';
+import { skip } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 describe('UserListUsecase', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let usecase: UserListUsecase;
+  let store: MockStore<{}>;
+  let repo: UserRepository;
 
-  it('should be created', () => {
-    const usecase: UserListUsecase = TestBed.get(UserListUsecase);
-    expect(usecase).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [UserRepository, provideMockStore()],
+    });
+
+    usecase = TestBed.get(UserListUsecase);
+    store = TestBed.get(Store);
+    repo = TestBed.get(UserRepository);
+  });
+
+  describe('call initialize()', () => {
+    let userList: User[];
+    beforeEach(() => {
+      userList = [
+        {
+          id: 1,
+          name: 'name',
+          username: 'username',
+          email: 'email',
+          address: { street: 'street', suite: 'suite', city: 'city', zipcode: 'zipcode', geo: { lat: 0, lng: 0 } },
+          phone: 'phone',
+          website: 'website',
+          company: { name: 'company-name', catchPhrase: 'company-catchPhrase', bs: 'company-bs' },
+        },
+      ];
+      spyOn(repo, 'fetchUserList').and.returnValue(of(userList));
+    });
+
+    it('userActions.saveUserList が dispatch されること', async () => {
+      const saveUserListAction = userListActions.saveUserList({ userList });
+      const expected = [saveUserListAction];
+
+      const actions: Action[] = [];
+      store.scannedActions$.pipe(skip(1)).subscribe((action) => actions.push(action));
+      await usecase.initialize();
+
+      expect(actions).toEqual(expected);
+    });
   });
 });
--- src/app/features/user-list/applications/user-list.usecase.ts
+++ src/app/features/user-list/applications/user-list.usecase.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { UserRepository } from '../../../infrastructures/repositories/user.repository';
+import { actions as userActions } from './user-list.store';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserListUsecase {
-  constructor() {}
+  constructor(private store$: Store<{}>, private userRepository: UserRepository) {}
+
+  async initialize() {
+    const result = await this.userRepository.fetchUserList().toPromise();
+    this.store$.dispatch(userActions.saveUserList({ userList: result }));
+  }
 }
