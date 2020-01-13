1cc1ef186fb7566c9126f070e4051ae3528b442e
Wataru.Kasahara <kasahara1019@gmail.com>
2020-01-13 12:47:35 +0900

    型の更新による spec の修正

--- src/app/features/user-list/applications/user-list.store.spec.ts
+++ src/app/features/user-list/applications/user-list.store.spec.ts
@@ -1,9 +1,21 @@
 import reducer, { actions, State } from './user-list.store';
+import { User } from '../domain/user-list';
 
 describe('userList reducer', () => {
   it('action type : saveUserList', () => {
-    const state: State = { userList: null };
-    const updatedState = null;
+    const updatedState: User[] = [
+      {
+        id: 1,
+        name: 'name',
+        username: 'username',
+        email: 'email',
+        address: { street: 'street', suite: 'suite', city: 'city', zipcode: 'zipcode', geo: { lat: 0, lng: 0 } },
+        phone: 'phone',
+        website: 'website',
+        company: { name: 'company-name', catchPhrase: 'company-catchPhrase', bs: 'company-bs' },
+      },
+    ];
+    const state: State = { userList: updatedState };
     const result = reducer(state, actions.saveUserList({ userList: updatedState }));
 
     expect(result).toEqual({ userList: updatedState });
