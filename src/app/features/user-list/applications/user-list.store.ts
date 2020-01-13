2e5f537be8fd9d733970425cfedd5f6687bb7ac6
Wataru.Kasahara <kasahara1019@gmail.com>
2020-01-13 12:13:40 +0900

    state に型情報を付与

--- src/app/features/user-list/applications/user-list.store.ts
+++ src/app/features/user-list/applications/user-list.store.ts
@@ -1,8 +1,9 @@
 import { createAction, createReducer, on, props, union } from '@ngrx/store';
+import { User } from '../domain/user-list';
 
 // NOTE: State
 export interface State {
-  userList: any | null;
+  userList: User[] | null;
 }
 
 export const initialState: State = {
@@ -10,7 +11,7 @@ export const initialState: State = {
 };
 
 // NOTE: Actions
-export const saveUserList = createAction('[UserList] save', props<{ userList: any }>());
+export const saveUserList = createAction('[UserList] save', props<{ userList: User[] }>());
 
 export const actions = { saveUserList };
 const actionsUnion = union(actions);
