cfd4fe80862cdabba21d6484b3c4a05a7bae02fd
Wataru.Kasahara <kasahara1019@gmail.com>
2020-01-14 09:36:00 +0900

    userList 用の feature store を登録

--- src/app/features/user-list/user-list.module.ts
+++ src/app/features/user-list/user-list.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserListComponent } from './views/user-list/user-list.component';
+import { StoreModule } from '@ngrx/store';
+import { default as reducer, featureName } from './applications/user-list.store';
 
 @NgModule({
   declarations: [UserListComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, StoreModule.forFeature(featureName, reducer)],
   exports: [UserListComponent],
 })
 export class UserListModule {}
