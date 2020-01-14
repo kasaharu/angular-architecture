e8af4adcfb39d974407ddc8ee42baa8729edd470
Wataru.Kasahara <kasahara1019@gmail.com>
2020-01-14 09:25:49 +0900

    store から userList を取得する query を実装

--- src/app/features/user-list/applications/user-list.query.spec.ts
+++ src/app/features/user-list/applications/user-list.query.spec.ts
@@ -1,16 +1,59 @@
 import { TestBed } from '@angular/core/testing';
-
+import { Store } from '@ngrx/store';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { User } from '../domain/user-list';
 import { UserListQuery } from './user-list.query';
+import * as UserListStore from './user-list.store';
+
+interface MockStoreType {
+  [UserListStore.featureName]: UserListStore.State;
+}
+
+const initialState: MockStoreType = {
+  [UserListStore.featureName]: {
+    userList: null,
+  },
+};
 
 describe('UserListQuery', () => {
   let query: UserListQuery;
+  let store$: MockStore<MockStoreType>;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideMockStore({ initialState })],
+    });
 
     query = TestBed.get(UserListQuery);
+    store$ = TestBed.get(Store);
   });
 
-  it('should be created', () => {
-    expect(query).toBeTruthy();
+  describe('userList$ の値のチェック', () => {
+    it('initialState は null であること', () => {
+      query.userList$.subscribe((val) => expect(val).toBeNull());
+    });
+
+    it('initialState は null であること', () => {
+      const userList: User[] = [
+        {
+          id: 1,
+          name: 'name',
+          username: 'username',
+          email: 'email',
+          address: { street: 'street', suite: 'suite', city: 'city', zipcode: 'zipcode', geo: { lat: 0, lng: 0 } },
+          phone: 'phone',
+          website: 'website',
+          company: { name: 'company-name', catchPhrase: 'company-catchPhrase', bs: 'company-bs' },
+        },
+      ];
+      const updateState: MockStoreType = {
+        ...initialState,
+        [UserListStore.featureName]: {
+          userList,
+        },
+      };
+      store$.setState(updateState);
+      query.userList$.subscribe((val) => expect(val).toEqual(userList));
+    });
   });
 });
--- src/app/features/user-list/applications/user-list.query.ts
+++ src/app/features/user-list/applications/user-list.query.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { User } from '../domain/user-list';
+import { selectStore, State } from './user-list.store';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserListQuery {
-  constructor() {}
+  constructor(private store$: Store<State>) {}
+
+  readonly userList$: Observable<User[] | null> = selectStore(this.store$, (state) => state.userList);
 }
