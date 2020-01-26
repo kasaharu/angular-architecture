f2e352c913797fdda00da1d163cb5e04400258d8
Wataru.Kasahara <kasahara1019@gmail.com>
2020-01-26 15:44:18 +0900

    yarn ng g c features/user/views/user

--- src/app/features/user/user.module.ts
+++ src/app/features/user/user.module.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { UserComponent } from './views/user/user.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [UserComponent],
   imports: [
     CommonModule,
   ],
---
+++ src/app/features/user/views/user/user.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
