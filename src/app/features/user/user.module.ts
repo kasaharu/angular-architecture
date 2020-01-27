3c709b0a5ffc4be23e8df433a5019b3a398a9078
Wataru.Kasahara <kasahara1019@gmail.com>
2020-01-27 22:47:11 +0900

    yarn ng g c features/user/views/user-form

--- src/app/features/user/user.module.ts
+++ src/app/features/user/user.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { default as reducer, featureName } from './applications/user.store';
+import { UserFormComponent } from './views/user-form/user-form.component';
 import { UserComponent } from './views/user/user.component';
 
 @NgModule({
-  declarations: [UserComponent],
+  declarations: [UserComponent, UserFormComponent],
   imports: [CommonModule, StoreModule.forFeature(featureName, reducer)],
   exports: [UserComponent],
 })
---
+++ src/app/features/user/views/user-form/user-form.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-form',
+  templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UserFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
