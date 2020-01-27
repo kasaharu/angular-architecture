304638819f646ed1817438d8aae71f06249b853b
Wataru.Kasahara <kasahara1019@gmail.com>
2020-01-27 22:23:42 +0900

    UserStore を merge

--- src/app/features/user/user.module.ts
+++ src/app/features/user/user.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { default as reducer, featureName } from './applications/user.store';
 import { UserComponent } from './views/user/user.component';
 
 @NgModule({
   declarations: [UserComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, StoreModule.forFeature(featureName, reducer)],
   exports: [UserComponent],
 })
 export class UserModule {}
