86a4e3751c2af7f814785f4f4a67bbf6b3de897c
Wataru.Kasahara <kasahara1019@gmail.com>
2020-01-27 22:45:08 +0900

    store から user を取得するための query method を実装

--- src/app/features/user/applications/user.query.spec.ts
+++ src/app/features/user/applications/user.query.spec.ts
@@ -1,16 +1,60 @@
 import { TestBed } from '@angular/core/testing';
-
+import { Store } from '@ngrx/store';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { User } from '../domain/user';
 import { UserQuery } from './user.query';
+import * as UserStore from './user.store';
+
+interface MockStoreType {
+  [UserStore.featureName]: UserStore.State;
+}
+
+const initialState: MockStoreType = {
+  [UserStore.featureName]: {
+    user: null,
+  },
+};
 
 describe('UserQuery', () => {
   let query: UserQuery;
+  let store$: MockStore<MockStoreType>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    query = TestBed.inject(UserQuery);
+    TestBed.configureTestingModule({
+      providers: [provideMockStore({ initialState })],
+    });
+    query = TestBed.get(UserQuery);
+    store$ = TestBed.get(Store);
   });
 
   it('should be created', () => {
     expect(query).toBeTruthy();
+  });
+
+  describe('user$ の値のチェック', () => {
+    it('initialState は null であること', () => {
+      query.user$.subscribe((val) => expect(val).toBeNull());
+    });
+
+    it('store が更新されたら更新された値が取得できること ', () => {
+      const user: User = {
+        id: 1,
+        name: 'name',
+        username: 'username',
+        email: 'email',
+        address: { street: 'street', suite: 'suite', city: 'city', zipcode: 'zipcode', geo: { lat: '0', lng: '0' } },
+        phone: 'phone',
+        website: 'website',
+        company: { name: 'company-name', catchPhrase: 'company-catchPhrase', bs: 'company-bs' },
+      };
+      const updateState: MockStoreType = {
+        ...initialState,
+        [UserStore.featureName]: {
+          user,
+        },
+      };
+      store$.setState(updateState);
+      query.user$.subscribe((val) => expect(val).toEqual(user));
+    });
   });
 });
--- src/app/features/user/applications/user.query.ts
+++ src/app/features/user/applications/user.query.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { User } from '../domain/user';
+import { selectStore, State } from './user.store';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserQuery {
-  constructor() {}
+  constructor(private store$: Store<State>) {}
+
+  readonly user$: Observable<User | null> = selectStore(this.store$, (state) => state.user);
 }
