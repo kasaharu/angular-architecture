35493e00a25c26e1aa9b14d96ea1304e24370df2
Wataru.Kasahara <kasahara1019@gmail.com>
2020-02-03 10:20:10 +0900

    Fix lint error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UserListComponent } from './features/user/views/pages/user-list/user-list.component';
 import { UserModule } from './features/user/user.module';
+import { UserListComponent } from './features/user/views/pages/user-list/user-list.component';
 import { UserComponent } from './features/user/views/user/user.component';
 
 const routes: Routes = [
