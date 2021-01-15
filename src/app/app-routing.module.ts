015f22a02f783ec13f20fd730ca7a513ef78a1ea
Wataru.Kasahara <kasahara1019@gmail.com>
2021-01-15 22:16:10 +0900

    feat: Heroes の表示を /heroes アクセス時に変更

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroesComponent } from './features/heroes/pages/heroes/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'heroes', component: HeroesComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
