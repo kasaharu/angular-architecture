2791e9c4e22f714a0a36c5f2aabfd36e6a4b1409
Wataru.Kasahara <kasahara1019@gmail.com>
2021-01-16 12:26:14 +0900

    feat: InMemoryWebApi の準備

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardModule } from './features/dashboard/dashboard.module';
 import { HeroesModule } from './features/heroes/heroes.module';
+import { InMemoryDataService } from './infrastructures/in-memory/in-memory-data.service';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HeroesModule, DashboardModule, SharedModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HeroesModule,
+    DashboardModule,
+    SharedModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
