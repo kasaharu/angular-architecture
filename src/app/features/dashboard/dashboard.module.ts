e44fd64c1fc7248a2fcf548c4dc0e0331259d309
Wataru.Kasahara <kasahara1019@gmail.com>
2021-01-16 11:03:59 +0900

    yarn ng g c features/dashboard/pages/dashboard

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [DashboardComponent],
   imports: [
     CommonModule
   ]
