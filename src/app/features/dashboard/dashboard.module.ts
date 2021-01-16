b242206f629ae24d548428640e2b75b6a5f7a934
Wataru.Kasahara <kasahara1019@gmail.com>
2021-01-16 11:55:27 +0900

    feat: Dashboard から HeroDetail への遷移ができるようにする

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
 })
 export class DashboardModule {}
--- src/app/features/hero-detail/pages/hero-detail/hero-detail.component.ts
+++ src/app/features/hero-detail/pages/hero-detail/hero-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from 'src/app/infrastructures/gateways/hero.service';
 import { Hero } from '../../../../domain/hero';
 
 @Component({
@@ -8,9 +10,20 @@ import { Hero } from '../../../../domain/hero';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeroDetailComponent implements OnInit {
-  constructor() {}
-  @Input()
+  constructor(private readonly _route: ActivatedRoute, private readonly _heroService: HeroService) {}
+
   hero: Hero | null = null;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = this._route.snapshot.paramMap.get('id');
+    if (id === null) {
+      // TOOD: null の場合のエラーハンドリングが必要
+      return;
+    }
+    this._heroService.getHero(+id).subscribe((hero) => (this.hero = hero));
+  }
 }
