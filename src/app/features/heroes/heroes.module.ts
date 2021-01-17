cbbbf5d57523b4c57fcd2f32173a7746f8b06635
Wataru.Kasahara <kasahara1019@gmail.com>
2021-01-17 12:43:17 +0900

    yarn ng g c features/heroes/containers/heroes

--- src/app/features/heroes/heroes.module.ts
+++ src/app/features/heroes/heroes.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeroDetailModule } from '../hero-detail/hero-detail.module';
 import { HeroesPageComponent } from './pages/heroes/heroes.component';
+import { HeroesComponent } from './containers/heroes/heroes.component';
 
 @NgModule({
-  declarations: [HeroesPageComponent],
+  declarations: [HeroesPageComponent, HeroesComponent],
   imports: [CommonModule, RouterModule, HeroDetailModule],
 })
 export class HeroesModule {}
