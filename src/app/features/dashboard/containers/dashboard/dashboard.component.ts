03e8e812c65cbd75840c2acea7be8374dbce7861
Wataru.Kasahara <kasahara1019@gmail.com>
2021-01-18 21:40:42 +0900

    refactor: Dashboard の表示を page component から container component に変更

--- src/app/features/dashboard/containers/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/containers/dashboard/dashboard.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Hero } from '../../../../domain/hero';
+import { HeroGateway } from '../../../../infrastructures/gateways/hero.gateway';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardComponent implements OnInit {
+  constructor(private readonly _heroService: HeroGateway) {}
 
-  constructor() { }
+  heroes: Hero[] | null = null;
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
+  getHeroes(): void {
+    this._heroService.getHeroes().subscribe((heroes) => {
+      this.heroes = heroes.slice(1, 5);
+    });
+  }
 }
--- src/app/features/dashboard/pages/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/pages/dashboard/dashboard.component.ts
@@ -1,25 +1,8 @@
-import { ThrowStmt } from '@angular/compiler';
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { Hero } from '../../../../domain/hero';
-import { HeroGateway } from '../../../../infrastructures/gateways/hero.gateway';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashboardPageComponent implements OnInit {
-  constructor(private readonly _heroService: HeroGateway) {}
-
-  heroes: Hero[] | null = null;
-
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-
-  getHeroes(): void {
-    this._heroService.getHeroes().subscribe((heroes) => {
-      this.heroes = heroes.slice(1, 5);
-    });
-  }
-}
+export class DashboardPageComponent {}
