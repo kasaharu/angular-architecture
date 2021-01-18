519fa1b082d45321b99e44e308148849264ac336
Wataru.Kasahara <kasahara1019@gmail.com>
2021-01-18 21:20:58 +0900

    yarn ng g c features/dashboard/containers/dashboard

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { DashboardPageComponent } from './pages/dashboard/dashboard.component';
+import { DashboardComponent } from './containers/dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [DashboardPageComponent],
+  declarations: [DashboardPageComponent, DashboardComponent],
   imports: [CommonModule, RouterModule],
 })
 export class DashboardModule {}
