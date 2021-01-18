7c6e95c148c2f060bc283d8e411136a6bc85dace
Wataru.Kasahara <kasahara1019@gmail.com>
2021-01-18 21:17:45 +0900

    refactor: Heroes を Observable にして変更があった場合に再描画するように修正

--- src/app/features/heroes/containers/heroes/heroes.component.ts
+++ src/app/features/heroes/containers/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { Hero } from '../../../../domain/hero';
 import { HeroGateway } from '../../../../infrastructures/gateways/hero.gateway';
 
@@ -11,13 +12,13 @@ import { HeroGateway } from '../../../../infrastructures/gateways/hero.gateway';
 export class HeroesComponent implements OnInit {
   constructor(private readonly _heroService: HeroGateway) {}
 
-  heroes: Hero[] | null = null;
+  heroes$ = new BehaviorSubject<Hero[] | null>(null);
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
   getHeroes(): void {
-    this._heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+    this._heroService.getHeroes().subscribe((heroes) => this.heroes$.next(heroes));
   }
 }
