2c53a5f35ca90fc31be816152a0d60c39aa23166
Wataru.Kasahara <kasahara1019@gmail.com>
2021-01-22 16:57:26 +0900

    feat: Dashboard feature の状態を管理する DashboardStore を用意

---
+++ src/app/features/dashboard/containers/dashboard/dashboard.store.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { ComponentStore } from '@ngrx/component-store';
+import { Observable } from 'rxjs';
+import { Hero } from '../../../../domain/hero';
+
+export interface DashboardState {
+  heroes: Hero[] | null;
+}
+
+@Injectable()
+export class DashboardStore extends ComponentStore<DashboardState> {
+  constructor() {
+    super({ heroes: null });
+  }
+
+  readonly heroes$: Observable<Hero[] | null> = this.select((state) => state.heroes);
+  readonly saveHeroes = this.updater((_, heroes: Hero[]) => ({ heroes }));
+}
