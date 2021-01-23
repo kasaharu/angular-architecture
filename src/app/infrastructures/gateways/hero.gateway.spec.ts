bf6bd9d246a7b05c4417c1fd21485a4516c7a513
Wataru.Kasahara <kasahara1019@gmail.com>
2021-01-23 11:09:50 +0900

    test: HeroGateway のテストケース追加

--- src/app/infrastructures/gateways/hero.gateway.spec.ts
+++ src/app/infrastructures/gateways/hero.gateway.spec.ts
@@ -1,18 +1,51 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { Hero } from '../../domain/hero';
 import { HeroGateway } from './hero.gateway';
 
 describe('HeroGateway', () => {
   let gateway: HeroGateway;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
     });
+
     gateway = TestBed.inject(HeroGateway);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(gateway).toBeTruthy();
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('getHeroes()', () => {
+    const expected: Hero[] = [
+      { id: 11, name: 'Dr Nice' },
+      { id: 12, name: 'Narco' },
+      { id: 13, name: 'Bombasto' },
+    ];
+
+    gateway.getHeroes().subscribe((resp) => {
+      expect(resp).toEqual(expected);
+    });
+
+    const req = httpTestingController.expectOne('api/heroes');
+    expect(req.request.method).toEqual('GET');
+    req.flush(expected);
+  });
+
+  it('getHero()', () => {
+    const expected: Hero = { id: 13, name: 'Bombasto' };
+    const targetId = expected.id;
+
+    gateway.getHero(targetId).subscribe((resp) => {
+      expect(resp).toEqual(expected);
+    });
+
+    const req = httpTestingController.expectOne(`api/heroes/${targetId}`);
+    expect(req.request.method).toEqual('GET');
+    req.flush(expected);
   });
 });
