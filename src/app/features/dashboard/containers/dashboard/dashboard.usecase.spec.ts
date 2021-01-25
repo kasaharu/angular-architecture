5051b7e0d6ba1c18b4040342bdc0ce17d2ad4f8b
Wataru.Kasahara <kasahara1019@gmail.com>
2021-01-25 21:22:22 +0900

    test: DashboarUsecase#fetchHeroes() のテストケース追加

--- src/app/features/dashboard/containers/dashboard/dashboard.usecase.spec.ts
+++ src/app/features/dashboard/containers/dashboard/dashboard.usecase.spec.ts
@@ -1,20 +1,51 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { Hero } from '../../../../domain/hero';
+import { HeroGateway } from '../../../../infrastructures/gateways/hero.gateway';
 import { DashboardStore } from './dashboard.store';
 import { DashboardUsecase } from './dashboard.usecase';
 
+class MockHeroGateway implements Partial<HeroGateway> {
+  getHeroes(): any {}
+}
+
 describe('DashboardUsecase', () => {
   let usecase: DashboardUsecase;
+  let componentStore: DashboardStore;
+  let gateway: HeroGateway;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      providers: [DashboardUsecase, DashboardStore],
+      providers: [DashboardUsecase, DashboardStore, { provide: HeroGateway, useClass: MockHeroGateway }],
     });
+
     usecase = TestBed.inject(DashboardUsecase);
+    componentStore = TestBed.inject(DashboardStore);
+    gateway = TestBed.inject(HeroGateway);
   });
 
-  it('should be created', () => {
-    expect(usecase).toBeTruthy();
+  describe('fetchHeroes', () => {
+    it('メソッドを呼んだときに componentStore.saveHeroes() が実行されること', async () => {
+      const heroes: Hero[] = [{ id: 1, name: 'hero name' }];
+      spyOn(gateway, 'getHeroes').and.returnValue(of(heroes));
+      spyOn(componentStore, 'saveHeroes');
+
+      await usecase.fetchHeroes();
+
+      expect(componentStore.saveHeroes).toHaveBeenCalledWith(heroes);
+    });
+
+    it('メソッドを呼んだときに componentStore の state に heroes がセットされること', async () => {
+      const heroes: Hero[] = [{ id: 1, name: 'hero name' }];
+      spyOn(gateway, 'getHeroes').and.returnValue(of(heroes));
+
+      await usecase.fetchHeroes();
+
+      componentStore
+        .select((state) => state.heroes)
+        .subscribe((h) => {
+          expect(h).toEqual(heroes);
+        });
+    });
   });
 });
