da873379390ca96c1f0017ce41f8e97b5a1dced6
Wataru.Kasahara <kasahara1019@gmail.com>
2021-01-26 21:38:15 +0900

    test: DashboardComponent のテストケース追加

--- src/app/features/dashboard/containers/dashboard/dashboard.component.spec.ts
+++ src/app/features/dashboard/containers/dashboard/dashboard.component.spec.ts
@@ -1,25 +1,71 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Hero } from '../../../../domain/hero';
 import { DashboardComponent } from './dashboard.component';
+import { DashboardStore } from './dashboard.store';
+import { DashboardUsecase } from './dashboard.usecase';
+
+class MockDashboardUsecase implements Partial<DashboardUsecase> {
+  fetchHeroes(): any {}
+}
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let usecase: DashboardUsecase;
+  let componentStore: DashboardStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [DashboardComponent],
-      imports: [HttpClientTestingModule],
-    }).compileComponents();
+    })
+      .overrideComponent(DashboardComponent, {
+        add: { providers: [DashboardStore, { provide: DashboardUsecase, useClass: MockDashboardUsecase }] },
+      })
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    usecase = fixture.debugElement.injector.get(DashboardUsecase);
+    componentStore = fixture.debugElement.injector.get(DashboardStore);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('ngOnInit', () => {
+    it('component が作成されたこと', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('component が作成されたときに usecase の fetchHeroes が呼ばれること', () => {
+      spyOn(usecase, 'fetchHeroes');
+      fixture.detectChanges();
+
+      expect(usecase.fetchHeroes).toHaveBeenCalled();
+    });
+  });
+
+  describe('componentStore に heroes がセットされている場合', () => {
+    it('6 人の hero がセットされる場合 4 人の hero が component.heroes にセットされる', () => {
+      const heroes: Hero[] = [
+        { id: 11, name: 'Dr Nice' },
+        { id: 12, name: 'Narco' },
+        { id: 13, name: 'Bombasto' },
+        { id: 14, name: 'Celeritas' },
+        { id: 15, name: 'Magneta' },
+        { id: 16, name: 'RubberMan' },
+      ];
+      const expected: Hero[] = [
+        { id: 12, name: 'Narco' },
+        { id: 13, name: 'Bombasto' },
+        { id: 14, name: 'Celeritas' },
+        { id: 15, name: 'Magneta' },
+      ];
+      componentStore.setState({ heroes });
+
+      component.heroes$.subscribe((state) => {
+        expect(state).toEqual(expected);
+      });
+    });
   });
 });
