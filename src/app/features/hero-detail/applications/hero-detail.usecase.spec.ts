46b4fce185b35d13eb52bbbdb64e1bbc8e8fc34c
Wataru.Kasahara <kasahara1019@gmail.com>
2021-01-27 22:18:11 +0900

    feat: HeroDetail feature の usecase を実装

--- src/app/features/hero-detail/applications/hero-detail.usecase.spec.ts
+++ src/app/features/hero-detail/applications/hero-detail.usecase.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HeroGateway } from 'src/app/infrastructures/gateways/hero.gateway';
+import { HeroDetailStore } from './hero-detail.store';
 import { HeroDetailUsecase } from './hero-detail.usecase';
+
+class MockHeroGateway implements Partial<HeroGateway> {}
 
 describe('HeroDetailUsecase', () => {
   let usecase: HeroDetailUsecase;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [HeroDetailStore, HeroDetailUsecase, { provide: HeroGateway, useClass: MockHeroGateway }],
+    });
     usecase = TestBed.inject(HeroDetailUsecase);
   });
 
--- src/app/features/hero-detail/applications/hero-detail.usecase.ts
+++ src/app/features/hero-detail/applications/hero-detail.usecase.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HeroGateway } from '../../../infrastructures/gateways/hero.gateway';
+import { HeroDetailStore } from './hero-detail.store';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable()
 export class HeroDetailUsecase {
-  constructor() {}
+  constructor(private readonly _componentStore: HeroDetailStore, private readonly _heroGateway: HeroGateway) {}
+
+  setHeroId(id: number) {
+    this._componentStore.setId(id);
+  }
+
+  async fetchHero(id: number): Promise<void> {
+    const hero = await this._heroGateway.getHero(id).toPromise();
+    this._componentStore.saveHero(hero);
+  }
 }
