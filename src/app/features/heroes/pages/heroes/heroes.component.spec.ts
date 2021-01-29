3263454797d3a57606d999aadaff7e3d9de628f5
Wataru.Kasahara <kasahara1019@gmail.com>
2021-01-29 21:57:34 +0900

    test: Delete unnecessary providers

--- src/app/features/heroes/pages/heroes/heroes.component.spec.ts
+++ src/app/features/heroes/pages/heroes/heroes.component.spec.ts
@@ -1,15 +1,6 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { Observable, of } from 'rxjs';
-import { Hero } from 'src/app/domain/hero';
-import { HeroGateway } from '../../../../infrastructures/gateways/hero.gateway';
 import { HeroesPageComponent } from './heroes.component';
-
-class MockHeroService implements Partial<HeroGateway> {
-  getHeroes(): Observable<Hero[]> {
-    return of([{ id: 1, name: 'Dr Nice' }]);
-  }
-}
 
 describe('HeroesPageComponent', () => {
   let component: HeroesPageComponent;
@@ -18,7 +9,6 @@ describe('HeroesPageComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HeroesPageComponent],
-      providers: [{ provide: HeroGateway, useClass: MockHeroService }],
       schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
   });
