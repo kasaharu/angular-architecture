41b5c7c1ce7b76e0692fe6a2e5d390a6b88b2ad3
Wataru.Kasahara <kasahara1019@gmail.com>
2021-01-30 11:27:34 +0900

    test: HeroesComponent のテストケース追加

--- src/app/features/heroes/containers/heroes/heroes.component.spec.ts
+++ src/app/features/heroes/containers/heroes/heroes.component.spec.ts
@@ -1,25 +1,43 @@
-import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HeroesStore } from '../../applications/heroes.store';
+import { HeroesUsecase } from '../../applications/heroes.usecase';
 import { HeroesComponent } from './heroes.component';
 
-let component: HeroesComponent;
-let fixture: ComponentFixture<HeroesComponent>;
+class MockHeroesUsecase implements Partial<HeroesUsecase> {
+  fetchHeroes(): any {}
+}
 
 describe('HeroesComponent', () => {
+  let component: HeroesComponent;
+  let fixture: ComponentFixture<HeroesComponent>;
+  let usecase: HeroesUsecase;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HeroesComponent],
-      schemas: [NO_ERRORS_SCHEMA],
-    });
+    })
+      .overrideComponent(HeroesComponent, {
+        add: { providers: [HeroesStore, { provide: HeroesUsecase, useClass: MockHeroesUsecase }] },
+      })
+      .compileComponents();
+  });
 
-    beforeEach(() => {
-      fixture = TestBed.createComponent(HeroesComponent);
-      component = fixture.componentInstance;
-      fixture.detectChanges();
-    });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeroesComponent);
+    component = fixture.componentInstance;
+    usecase = fixture.debugElement.injector.get(HeroesUsecase);
+  });
 
-    it('should create', () => {
+  describe('ngOnInit', () => {
+    it('component が作成されたこと', () => {
       expect(component).toBeTruthy();
+    });
+
+    it('component が作成されたときに usecase の fetchHeroes が呼ばれること', () => {
+      spyOn(usecase, 'fetchHeroes');
+      fixture.detectChanges();
+
+      expect(usecase.fetchHeroes).toHaveBeenCalled();
     });
   });
 });
