2cc604409c126bfe1269bca9fd9a4904e8c98d9e
Wataru.Kasahara <kasahara1019@gmail.com>
2021-09-30 08:55:14 +0900

    fix(mock): handle する api のパスが間違っていたので修正

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,7 @@
 import { rest } from 'msw';
 
 export const handlers = [
-  rest.get('/heroes', (req, res, ctx) => {
+  rest.get('/api/heroes', (req, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.json([
