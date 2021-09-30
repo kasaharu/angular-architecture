40ede443237343aabf3f2120d035861279342bfd
Wataru.Kasahara <kasahara1019@gmail.com>
2021-09-30 09:04:33 +0900

    fix(mock): AppModule での HttpClientInMemoryWebApiModule の読み込みを削除

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardModule } from './features/dashboard/dashboard.module';
 import { HeroDetailModule } from './features/hero-detail/hero-detail.module';
 import { HeroesModule } from './features/heroes/heroes.module';
-import { InMemoryDataService } from './data-access/in-memory/in-memory-data.service';
 import { SharedModule } from './shared/shared.module';
 import { HeadingModule } from './ui/heading/heading.module';
 
@@ -21,7 +19,6 @@ import { HeadingModule } from './ui/heading/heading.module';
     HeroesModule,
     HeroDetailModule,
     SharedModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false }),
     HeadingModule,
   ],
   providers: [],
