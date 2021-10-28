46eac2d59a534a656cf49c3a7e086a50c46da97a
Wataru.Kasahara <kasahara1019@gmail.com>
2021-10-28 09:22:32 +0900

    yarn ng g c features/dashboard/views/ly-dashboard

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { DashboardPageComponent } from './pages/dashboard/dashboard.component';
 import { DashboardComponent } from './containers/dashboard/dashboard.component';
 import { DashboardContentsComponent } from './views/dashboard-contents/dashboard-contents.component';
+import { LyDashboardComponent } from './views/ly-dashboard/ly-dashboard.component';
 
 @NgModule({
-  declarations: [DashboardPageComponent, DashboardComponent, DashboardContentsComponent],
+  declarations: [DashboardPageComponent, DashboardComponent, DashboardContentsComponent, LyDashboardComponent],
   imports: [CommonModule, RouterModule],
 })
 export class DashboardModule {}
---
+++ src/app/features/dashboard/views/ly-dashboard/ly-dashboard.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-ly-dashboard',
+  templateUrl: './ly-dashboard.component.html',
+  styleUrls: ['./ly-dashboard.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LyDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
