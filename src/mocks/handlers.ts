c619e99ece0398f43b2644abb639ad95c61838bd
Wataru.Kasahara <kasahara1019@gmail.com>
2021-10-30 11:33:53 +0900

    feat(mock): 指定した id の hero を返す設定を追加

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,21 +1,24 @@
 import { rest } from 'msw';
 
+const heroes = [
+  { id: 11, name: 'Dr Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' },
+];
+
 export const handlers = [
   rest.get('/api/heroes', (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json([
-        { id: 11, name: 'Dr Nice' },
-        { id: 12, name: 'Narco' },
-        { id: 13, name: 'Bombasto' },
-        { id: 14, name: 'Celeritas' },
-        { id: 15, name: 'Magneta' },
-        { id: 16, name: 'RubberMan' },
-        { id: 17, name: 'Dynama' },
-        { id: 18, name: 'Dr IQ' },
-        { id: 19, name: 'Magma' },
-        { id: 20, name: 'Tornado' },
-      ]),
-    );
+    return res(ctx.status(200), ctx.json(heroes));
+  }),
+  rest.get('/api/heroes/:heroId', (req, res, ctx) => {
+    const { heroId } = req.params;
+    return res(ctx.status(200), ctx.json(heroes.find((hero) => hero.id === Number(heroId))));
   }),
 ];
