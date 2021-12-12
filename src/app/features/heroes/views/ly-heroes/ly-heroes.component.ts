a71e8628cca4a42a4900f8b08be4485068c97cb9
Wataru.Kasahara <kasahara1019@gmail.com>
2021-12-12 14:16:12 +0900

    refactor(heroes): page component の表示に関する部分を view component に委譲

--- src/app/features/heroes/views/ly-heroes/ly-heroes.component.ts
+++ src/app/features/heroes/views/ly-heroes/ly-heroes.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Hero } from '../../../../domain/hero';
 
 @Component({
   selector: 'app-ly-heroes',
   templateUrl: './ly-heroes.component.html',
   styleUrls: ['./ly-heroes.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LyHeroesComponent implements OnInit {
+  @Input() heroes: Hero[] | null = null;
+  @Output() heroAdded = new EventEmitter<string>();
+  @Output() heroDeleted = new EventEmitter<Hero>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  add(heroName: string): void {
+    this.heroAdded.emit(heroName);
   }
 
+  delete(hero: Hero): void {
+    this.heroDeleted.emit(hero);
+  }
 }
