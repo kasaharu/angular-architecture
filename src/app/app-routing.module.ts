607f4ac7d60aefd5471a908e6f5b9260bfbb6c24
Wataru.Kasahara <kasahara1019@gmail.com>
2021-12-12 13:57:17 +0900

    refactor(heroes): ページ表示時の root component は pages に移動

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './features/dashboard/pages/dashboard/dashboard.component';
 import { HeroDetailPageComponent } from './features/hero-detail/pages/hero-detail/hero-detail.component';
-import { HeroesComponent } from './features/heroes/containers/heroes/heroes.component';
+import { HeroesComponent } from './features/heroes/pages/heroes/heroes.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
--- src/app/features/heroes/heroes.module.ts
+++ src/app/features/heroes/heroes.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { HeroesComponent } from './containers/heroes/heroes.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
 import { HeroCreatorComponent } from './views/hero-creator/hero-creator.component';
 
 @NgModule({
