688cf59363715fd700c6c9d47720c4aaa4e0831c
Wataru.Kasahara <kasahara1019@gmail.com>
2022-01-05 09:04:48 +0900

    test: 不要になった module の import をやめる

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,12 +2,11 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { HeroesModule } from './features/heroes/heroes.module';
-import { SharedModule } from './shared/shared.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, SharedModule, HeroesModule],
+      imports: [RouterTestingModule, HeroesModule],
       declarations: [AppComponent],
     }).compileComponents();
   });
