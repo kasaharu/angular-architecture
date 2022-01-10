25c029fbcb78619a8b87ae9674536f5e4cb1fc9c
Wataru.Kasahara <kasahara1019@gmail.com>
2022-01-10 16:51:25 +0900

    refactor: MessageService のデータの持ち方を修正

--- src/app/shared/messages/services/message.service.ts
+++ src/app/shared/messages/services/message.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MessageService {
-  constructor() {}
+  private _messages$ = new BehaviorSubject<string[]>([]);
 
-  messages: string[] = [];
+  get messages$(): Observable<string[]> {
+    return this._messages$.asObservable();
+  }
 
   add(message: string): void {
-    this.messages.push(message);
+    this._messages$.next(this._messages$.value.concat(message));
   }
 
   clear(): void {
-    this.messages = [];
+    this._messages$.next([]);
   }
 }
