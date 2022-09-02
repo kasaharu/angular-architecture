3d11503e321965973e61d0451d13e51c5d684356
Wataru.Kasahara <kasahara1019@gmail.com>
2022-09-02 22:45:43 +0900

    yarn ng g c features/heroes/pages/heroes

---
+++ src/app/features/heroes/pages/heroes/heroes.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
