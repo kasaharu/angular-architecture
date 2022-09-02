22a0e41642568d98718edc5ded3394a59175f889
Wataru.Kasahara <kasahara1019@gmail.com>
2022-09-02 22:50:13 +0900

    feat: root path で HeroesComponent を表示

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './features/heroes/pages/heroes/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: HeroesComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
