c8ae0ad938b7b9e58645e7910897ae724544fe8b
Wataru.Kasahara <kasahara1019@gmail.com>
2022-09-05 22:34:00 +0900

    feat: LyHeroesComponent を表示

--- src/app/features/heroes/pages/heroes/heroes.component.ts
+++ src/app/features/heroes/pages/heroes/heroes.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { LyHeroesComponent } from '../../views/ly-heroes/ly-heroes.component';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LyHeroesComponent],
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss'],
 })
