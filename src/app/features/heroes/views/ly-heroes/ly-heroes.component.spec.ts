4f2e907b64c68b9ac8ccfbbc9854ce8f09e832a1
Wataru.Kasahara <kasahara1019@gmail.com>
2022-09-05 22:27:57 +0900

    yarn ng g c features/heroes/views/ly-heroes

---
+++ src/app/features/heroes/views/ly-heroes/ly-heroes.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LyHeroesComponent } from './ly-heroes.component';
+
+describe('LyHeroesComponent', () => {
+  let component: LyHeroesComponent;
+  let fixture: ComponentFixture<LyHeroesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ LyHeroesComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LyHeroesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/features/heroes/views/ly-heroes/ly-heroes.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-ly-heroes',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ly-heroes.component.html',
+  styleUrls: ['./ly-heroes.component.scss']
+})
+export class LyHeroesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
