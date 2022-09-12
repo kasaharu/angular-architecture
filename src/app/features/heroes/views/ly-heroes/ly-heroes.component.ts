f3eaab6576ab42650531907b12484aa194a92587
Wataru.Kasahara <kasahara1019@gmail.com>
2022-09-12 21:33:17 +0900

    feat: 双方向データバインディング構文を使って Hero の名前を変更できるようにした

--- src/app/features/heroes/views/ly-heroes/ly-heroes.component.ts
+++ src/app/features/heroes/views/ly-heroes/ly-heroes.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Hero } from '../../../../domain/hero';
 
 @Component({
   selector: 'app-ly-heroes',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './ly-heroes.component.html',
   styleUrls: ['./ly-heroes.component.scss'],
 })
