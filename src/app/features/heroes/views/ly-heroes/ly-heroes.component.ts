2a82b604d88028b9c84fcd29e98c537c2631525c
Wataru.Kasahara <kasahara1019@gmail.com>
2022-09-18 09:50:22 +0900

    feat: ボタンクリックでクリックした hero の情報を表示

--- src/app/features/heroes/views/ly-heroes/ly-heroes.component.ts
+++ src/app/features/heroes/views/ly-heroes/ly-heroes.component.ts
@@ -12,9 +12,10 @@ import { HEROES } from '../../../../mock-heroes';
   styleUrls: ['./ly-heroes.component.scss'],
 })
 export class LyHeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
   heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
