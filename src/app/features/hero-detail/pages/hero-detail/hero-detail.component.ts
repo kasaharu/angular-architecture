7acaeafec9769b920d0fef14b1bb32fb8954d727
Wataru.Kasahara <kasahara1019@gmail.com>
2022-09-19 08:10:59 +0900

    yarn ng g c features/hero-detail/pages/hero-detail

---
+++ src/app/features/hero-detail/pages/hero-detail/hero-detail.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-hero-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.scss']
+})
+export class HeroDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
