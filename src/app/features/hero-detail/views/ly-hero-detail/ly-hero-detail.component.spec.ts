4f523ce8bda00d7f663481f8136f9f1bd86d5fc2
Wataru.Kasahara <kasahara1019@gmail.com>
2022-09-19 09:05:27 +0900

    yarn ng g c features/hero-detail/views/ly-hero-detail

---
+++ src/app/features/hero-detail/views/ly-hero-detail/ly-hero-detail.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LyHeroDetailComponent } from './ly-hero-detail.component';
+
+describe('LyHeroDetailComponent', () => {
+  let component: LyHeroDetailComponent;
+  let fixture: ComponentFixture<LyHeroDetailComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ LyHeroDetailComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LyHeroDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/features/hero-detail/views/ly-hero-detail/ly-hero-detail.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-ly-hero-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ly-hero-detail.component.html',
+  styleUrls: ['./ly-hero-detail.component.scss']
+})
+export class LyHeroDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
