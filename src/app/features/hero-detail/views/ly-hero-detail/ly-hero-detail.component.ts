e9c2732e89f3731e6164fbc42cb3c939bd54efeb
Wataru.Kasahara <kasahara1019@gmail.com>
2022-09-19 09:19:45 +0900

    refactor: hero の詳細を hero-detail に切り出し

--- src/app/features/hero-detail/views/ly-hero-detail/ly-hero-detail.component.ts
+++ src/app/features/hero-detail/views/ly-hero-detail/ly-hero-detail.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Hero } from '../../../../domain/hero';
 
 @Component({
   selector: 'app-ly-hero-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './ly-hero-detail.component.html',
   styleUrls: ['./ly-hero-detail.component.scss'],
 })
 export class LyHeroDetailComponent implements OnInit {
   constructor() {}
+  @Input() hero?: Hero;
 
   ngOnInit(): void {}
 }
--- src/app/features/heroes/views/ly-heroes/ly-heroes.component.ts
+++ src/app/features/heroes/views/ly-heroes/ly-heroes.component.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { Hero } from '../../../../domain/hero';
+import { LyHeroDetailComponent } from '../../../../features/hero-detail/views/ly-hero-detail/ly-hero-detail.component';
 import { HEROES } from '../../../../mock-heroes';
 
 @Component({
   selector: 'app-ly-heroes',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, LyHeroDetailComponent],
   templateUrl: './ly-heroes.component.html',
   styleUrls: ['./ly-heroes.component.scss'],
 })
