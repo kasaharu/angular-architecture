194dd106e83a890af7b9754c432461e08e082d9b
Wataru.Kasahara <kasahara1019@gmail.com>
2022-09-26 11:26:35 +0900

    feat: MessagesComponent の配置

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MessagesComponent } from './shared/components/messages/messages.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MessagesComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
