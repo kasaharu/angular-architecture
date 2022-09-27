1b1a865e66ae5fec6b3707addc79e6d4c5bf5241
Wataru.Kasahara <kasahara1019@gmail.com>
2022-09-27 15:58:28 +0900

    feat: Hero 一覧取得時にメッセージを追加

--- src/app/infrastructures/api/hero.service.ts
+++ src/app/infrastructures/api/hero.service.ts
@@ -2,12 +2,16 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Hero } from '../../domain/hero';
 import { HEROES } from '../../mock-heroes';
+import { MessageService } from '../../shared/services/message.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
+  constructor(private readonly _messageService: MessageService) {}
+
   getHeroes(): Observable<Hero[]> {
+    this._messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
