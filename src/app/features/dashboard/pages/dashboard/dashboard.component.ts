6a131f1d08eb2d1d76cdad26eda8e55f4c4521d1
Wataru.Kasahara <kasahara1019@gmail.com>
2022-09-28 10:59:32 +0900

    feat: ダッシュボードを表示

--- src/app/features/dashboard/pages/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../../../domain/hero';
+import { HeroService } from '../../../../infrastructures/api/hero.service';
+import { LyDashboardComponent } from '../../views/ly-dashboard/ly-dashboard.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LyDashboardComponent],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardPageComponent implements OnInit {
-  constructor() {}
+  constructor(private readonly _heroService: HeroService) {}
+  heroes: Hero[] = [];
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this._heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes.slice(1, 5)));
+  }
 }
--- src/app/features/dashboard/views/ly-dashboard/ly-dashboard.component.ts
+++ src/app/features/dashboard/views/ly-dashboard/ly-dashboard.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Hero } from '../../../../domain/hero';
 
 @Component({
   selector: 'app-ly-dashboard',
@@ -8,8 +9,6 @@ import { CommonModule } from '@angular/common';
   templateUrl: './ly-dashboard.component.html',
   styleUrls: ['./ly-dashboard.component.scss'],
 })
-export class LyDashboardComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class LyDashboardComponent {
+  @Input() heroes: Hero[] = [];
 }
