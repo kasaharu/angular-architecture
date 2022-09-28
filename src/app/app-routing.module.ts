8e652bb0896cc54b55509e5f9570658a536d8195
Wataru.Kasahara <kasahara1019@gmail.com>
2022-09-28 10:52:18 +0900

    feat: /heroes にアクセスすることでダッシュボードを表示する

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardPageComponent } from './features/dashboard/pages/dashboard/dashboard.component';
 import { HeroesPageComponent } from './features/heroes/pages/heroes/heroes.component';
 
-const routes: Routes = [{ path: 'heroes', component: HeroesPageComponent }];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardPageComponent },
+  { path: 'heroes', component: HeroesPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
