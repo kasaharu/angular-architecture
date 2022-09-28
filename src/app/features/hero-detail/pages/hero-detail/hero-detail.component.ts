aa0e2e2186a8020e0bd9482ea522d4df266b55e1
Wataru.Kasahara <kasahara1019@gmail.com>
2022-09-28 12:05:26 +0900

    feat: 指定した id を持つ hero の詳細を表示

--- src/app/features/hero-detail/pages/hero-detail/hero-detail.component.ts
+++ src/app/features/hero-detail/pages/hero-detail/hero-detail.component.ts
@@ -1,11 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Hero } from '../../../../domain/hero';
+import { HeroService } from '../../../../infrastructures/api/hero.service';
+import { LyHeroDetailComponent } from '../../views/ly-hero-detail/ly-hero-detail.component';
 
 @Component({
   selector: 'app-hero-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LyHeroDetailComponent],
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.scss'],
 })
-export class HeroDetailPageComponent {}
+export class HeroDetailPageComponent implements OnInit {
+  constructor(private readonly _route: ActivatedRoute, private readonly _heroService: HeroService) {}
+  hero!: Hero;
+
+  ngOnInit(): void {
+    const id = Number(this._route.snapshot.paramMap.get('id'));
+    this.getHero(id);
+  }
+
+  getHero(id: number): void {
+    this._heroService.getHero(id).subscribe((hero) => (this.hero = hero));
+  }
+}
