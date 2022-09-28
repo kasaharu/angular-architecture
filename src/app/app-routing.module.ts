12683b80d431a22f7de16baf783a1d2d18f038ef
Wataru.Kasahara <kasahara1019@gmail.com>
2022-09-28 10:46:57 +0900

    feat: /heroes にアクセスすることで Hero 一覧を表示する

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesPageComponent } from './features/heroes/pages/heroes/heroes.component';
 
-const routes: Routes = [{ path: '', component: HeroesPageComponent }];
+const routes: Routes = [{ path: 'heroes', component: HeroesPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
