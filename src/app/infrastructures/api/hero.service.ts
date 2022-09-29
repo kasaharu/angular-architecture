7eb80eca2c7201dd6e4fe5d1cba6e1a10ad95f65
Wataru.Kasahara <kasahara1019@gmail.com>
2022-09-29 14:48:06 +0900

    feat: Hero 一覧を取得するロジックのエラーハンドリングを実装

--- src/app/infrastructures/api/hero.service.ts
+++ src/app/infrastructures/api/hero.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { catchError, Observable, of, tap } from 'rxjs';
 import { Hero } from '../../domain/hero';
 import { HEROES } from '../../mock-heroes';
 import { MessageService } from '../../shared/services/message.service';
@@ -14,13 +14,27 @@ export class HeroService {
   private _heroesUrl = 'api/heroes';
 
   getHeroes(): Observable<Hero[]> {
-    this._messageService.add('HeroService: fetched heroes');
-    return this._http.get<Hero[]>(this._heroesUrl);
+    return this._http.get<Hero[]>(this._heroesUrl).pipe(
+      tap((_) => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', [])),
+    );
   }
 
   getHero(id: number): Observable<Hero> {
     const hero = HEROES.find((h) => h.id === id)!;
     this._messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
+  }
+
+  private log(message: string): void {
+    this._messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
   }
 }
