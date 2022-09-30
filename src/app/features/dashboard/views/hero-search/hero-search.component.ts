d086ccce513969b348793834f87672a3c33dd2eb
Wataru.Kasahara <kasahara1019@gmail.com>
2022-09-30 10:29:05 +0900

    feat: ダッシュボードに検索機能を追加

--- src/app/features/dashboard/views/hero-search/hero-search.component.ts
+++ src/app/features/dashboard/views/hero-search/hero-search.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { debounceTime, distinctUntilChanged, Observable, Subject, switchMap } from 'rxjs';
+import { Hero } from '../../../../domain/hero';
+import { HeroService } from '../../../../infrastructures/api/hero.service';
 
 @Component({
   selector: 'app-hero-search',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './hero-search.component.html',
   styleUrls: ['./hero-search.component.scss'],
 })
 export class HeroSearchComponent implements OnInit {
-  constructor() {}
+  constructor(private readonly _heroService: HeroService) {}
 
-  ngOnInit(): void {}
+  heroes$!: Observable<Hero[]>;
+  private searchTerms = new Subject<string>();
+
+  ngOnInit(): void {
+    this.heroes$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this._heroService.searchHeroes(term)),
+    );
+  }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
 }
