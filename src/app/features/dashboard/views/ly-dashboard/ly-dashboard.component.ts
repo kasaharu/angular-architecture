9943f3eb647de22524f0d58ab0d5105768af14ba
Wataru.Kasahara <kasahara1019@gmail.com>
2022-09-30 10:06:28 +0900

    feat: dashboard に hero-search を表示

--- src/app/features/dashboard/views/ly-dashboard/ly-dashboard.component.ts
+++ src/app/features/dashboard/views/ly-dashboard/ly-dashboard.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Hero } from '../../../../domain/hero';
+import { HeroSearchComponent } from '../hero-search/hero-search.component';
 
 @Component({
   selector: 'app-ly-dashboard',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, HeroSearchComponent],
   templateUrl: './ly-dashboard.component.html',
   styleUrls: ['./ly-dashboard.component.scss'],
 })
