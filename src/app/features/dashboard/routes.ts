f32fdf808066bb1dca443d77e79acb7d9b141a1f
Wataru.Kasahara <kasahara1019@gmail.com>
2023-02-06 21:14:50 +0900

    feat: dashboard feature を lazy load にした

---
+++ src/app/features/dashboard/routes.ts
@@ -0,0 +1,5 @@
+import { Route } from '@angular/router';
+import { DashboardPageComponent } from './pages/dashboard/dashboard.component';
+
+const DASHBOARD_ROUTES: Route[] = [{ path: '', component: DashboardPageComponent }];
+export default DASHBOARD_ROUTES;
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
-import { DashboardPageComponent } from './features/dashboard/pages/dashboard/dashboard.component';
 import { HeroDetailPageComponent } from './features/hero-detail/pages/hero-detail/hero-detail.component';
 import { HeroesPageComponent } from './features/heroes/pages/heroes/heroes.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardPageComponent },
+  { path: 'dashboard', loadChildren: () => import('./features/dashboard/routes') },
   { path: 'detail/:id', component: HeroDetailPageComponent },
   { path: 'heroes', component: HeroesPageComponent },
 ];
