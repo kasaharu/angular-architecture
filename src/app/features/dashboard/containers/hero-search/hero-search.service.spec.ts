7139ce681d1fc49ea1d90de1d940a02cb50e64bd
Wataru.Kasahara <kasahara1019@gmail.com>
2023-02-21 22:34:03 +0900

    test(dashboard): テストの依存を解決

--- src/app/features/dashboard/containers/hero-search/hero-search.service.spec.ts
+++ src/app/features/dashboard/containers/hero-search/hero-search.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HeroService } from '../../../../infrastructures/api/hero.service';
 import { HeroSearchService } from './hero-search.service';
+
+class MockHeroSearchService {}
 
 describe('HeroSearchService', () => {
   let service: HeroSearchService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [HeroSearchService, { provide: HeroService, useClass: MockHeroSearchService }],
+    });
     service = TestBed.inject(HeroSearchService);
   });
 
