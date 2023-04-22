e312b0800b3c2e7831175b4bf7d600143f91cf67
Wataru.Kasahara <kasahara1019@gmail.com>
2023-04-22 11:44:50 +0900

    test(dashboard): page component が深い依存を気にしないように子 component を mock に置き換え

--- src/app/features/dashboard/pages/dashboard/dashboard.component.spec.ts
+++ src/app/features/dashboard/pages/dashboard/dashboard.component.spec.ts
@@ -1,12 +1,13 @@
+import { Component } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
 import { RouterTestingHarness } from '@angular/router/testing';
-import { HeroApi } from '../../../../infrastructures/api/hero.api';
 import { DashboardComponent } from '../../containers/dashboard/dashboard.component';
 import DASHBOARD_ROUTES from '../../routes';
 import { DashboardPageComponent } from './dashboard.component';
 
-class MockHeroService {}
+@Component({ selector: 'app-dashboard', standalone: true, template: '' })
+class MockDashboardComponent {}
 
 describe('DashboardPageComponent', () => {
   let harness: RouterTestingHarness;
@@ -15,8 +16,10 @@ describe('DashboardPageComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [DashboardPageComponent, DashboardComponent],
-      providers: [provideRouter(DASHBOARD_ROUTES), { provide: HeroApi, useClass: MockHeroService }],
-    }).compileComponents();
+      providers: [provideRouter(DASHBOARD_ROUTES)],
+    })
+      .overrideComponent(DashboardPageComponent, { remove: { imports: [DashboardComponent] }, add: { imports: [MockDashboardComponent] } })
+      .compileComponents();
 
     harness = await RouterTestingHarness.create();
     component = await harness.navigateByUrl('', DashboardPageComponent);
