ec8967704bcddd622ff92376ae8bd2c6b0ab359b
Wataru.Kasahara <kasahara1019@gmail.com>
2023-04-22 11:24:23 +0900

    test(dashboard): page component のテストを RouterTestingHarness を使って書き換え

--- src/app/features/dashboard/pages/dashboard/dashboard.component.spec.ts
+++ src/app/features/dashboard/pages/dashboard/dashboard.component.spec.ts
@@ -1,23 +1,27 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { RouterTestingHarness } from '@angular/router/testing';
 import { HeroApi } from '../../../../infrastructures/api/hero.api';
 import { DashboardComponent } from '../../containers/dashboard/dashboard.component';
+import DASHBOARD_ROUTES from '../../routes';
 import { DashboardPageComponent } from './dashboard.component';
 
 class MockHeroService {}
 
 describe('DashboardPageComponent', () => {
+  let harness: RouterTestingHarness;
   let component: DashboardPageComponent;
-  let fixture: ComponentFixture<DashboardPageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [DashboardPageComponent, DashboardComponent],
-      providers: [{ provide: HeroApi, useClass: MockHeroService }],
+      providers: [provideRouter(DASHBOARD_ROUTES), { provide: HeroApi, useClass: MockHeroService }],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(DashboardPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    harness = await RouterTestingHarness.create();
+    component = await harness.navigateByUrl('', DashboardPageComponent);
+
+    harness.detectChanges();
   });
 
   it('should create', () => {
