66b3a12cbba6abe519e7918fe31123341e7f5139
Wataru.Kasahara <kasahara1019@gmail.com>
2023-05-07 17:01:14 +0900

    refactor(dashboard): 不要になった処理を削除

--- src/app/features/dashboard/containers/dashboard/dashboard.store.spec.ts
+++ src/app/features/dashboard/containers/dashboard/dashboard.store.spec.ts
@@ -1,19 +1,13 @@
 import { TestBed } from '@angular/core/testing';
-import { HeroApi } from '../../../../infrastructures/api/hero.api';
 import { DashboardStore } from './dashboard.store';
-
-class MockHeroService implements Partial<HeroApi> {
-  getHeroes(): any {}
-}
 
 describe('DashboardStore', () => {
   let store: DashboardStore;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [DashboardStore, { provide: HeroApi, useClass: MockHeroService }],
+      providers: [DashboardStore],
     });
-
     store = TestBed.inject(DashboardStore);
   });
 
