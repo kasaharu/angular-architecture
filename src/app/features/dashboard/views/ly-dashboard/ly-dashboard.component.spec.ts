584a294a9bfc85bc6d93bb6320f3350dd7665f27
Wataru.Kasahara <kasahara1019@gmail.com>
2023-05-30 00:10:32 +0900

    test(dashboard): provider に渡す値を ApplicationConfig に変換

--- src/app/features/dashboard/views/ly-dashboard/ly-dashboard.component.spec.ts
+++ src/app/features/dashboard/views/ly-dashboard/ly-dashboard.component.spec.ts
@@ -1,3 +1,4 @@
+import { ApplicationConfig } from '@angular/core';
 import { By } from '@angular/platform-browser';
 import { render, screen } from '@testing-library/angular';
 import { Hero } from '../../../../domain/hero';
@@ -8,27 +9,27 @@ import { LyDashboardComponent } from './ly-dashboard.component';
 class MockHeroService {}
 
 describe('LyDashboardComponent', () => {
-  const provideHeroService = [{ provide: HeroApi, useClass: MockHeroService }];
+  const appConfig: ApplicationConfig = { providers: [{ provide: HeroApi, useClass: MockHeroService }] };
 
   describe('hero 一覧の表示', () => {
     it('heroes が Input で渡されたとき hero 一覧のリンクが表示されること', async () => {
       const heroName = 'test hero';
       const heroes: Hero[] = [{ id: 1, name: heroName }];
-      await render(LyDashboardComponent, { providers: provideHeroService, componentProperties: { heroes } });
+      await render(LyDashboardComponent, { ...appConfig, componentProperties: { heroes } });
 
       expect(screen.getByRole('link', { name: heroName, suggest: true })).toBeTruthy();
     });
 
     it('空の heroes が Input で渡されたとき hero 一覧のリンクが表示されないこと', async () => {
       const heroes: Hero[] = [];
-      const { queryByRole } = await render(LyDashboardComponent, { providers: provideHeroService, componentProperties: { heroes } });
+      const { queryByRole } = await render(LyDashboardComponent, { ...appConfig, componentProperties: { heroes } });
 
       expect(queryByRole('link')).toBe(null);
     });
   });
 
   it('HeroSearchComponent が表示されること', async () => {
-    const { fixture } = await render(LyDashboardComponent, { providers: provideHeroService });
+    const { fixture } = await render(LyDashboardComponent, { ...appConfig });
     const heroSearchElem = fixture.debugElement.query(By.directive(HeroSearchComponent));
 
     expect(heroSearchElem).toBeTruthy();
