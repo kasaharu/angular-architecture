346c56b29edf96c2c0b6cc4c1cdf72212a46ab66
Wataru.Kasahara <kasahara1019@gmail.com>
2023-08-12 14:20:08 +0900

    feat: heroe-detail の状態管理を ComponentStore から Signals に変更

--- src/app/features/hero-detail/pages/hero-detail/hero-detail.component.ts
+++ src/app/features/hero-detail/pages/hero-detail/hero-detail.component.ts
@@ -1,26 +1,27 @@
-import { AsyncPipe } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, computed, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Hero } from '../../../../domain/hero';
 import { LyHeroDetailComponent } from '../../views/ly-hero-detail/ly-hero-detail.component';
-import { HeroDetailStore } from './hero-detail.store';
+import { HeroDetailService } from './hero-detail.service';
 
 @Component({
   standalone: true,
-  imports: [AsyncPipe, LyHeroDetailComponent],
+  imports: [LyHeroDetailComponent],
   templateUrl: './hero-detail.component.html',
-  providers: [HeroDetailStore],
+  providers: [HeroDetailService],
 })
 export class HeroDetailPageComponent implements OnInit {
-  constructor(private readonly _route: ActivatedRoute, private readonly _componentStore: HeroDetailStore) {}
-  hero$ = this._componentStore.hero$;
+  private readonly _route = inject(ActivatedRoute);
+  private readonly _service = inject(HeroDetailService);
+
+  $hero = computed(() => this._service.$state().hero);
 
   ngOnInit(): void {
     const id = Number(this._route.snapshot.paramMap.get('id'));
-    this._componentStore.getHero(id);
+    this._service.getHero(id);
   }
 
   updateHero(hero: Hero): void {
-    this._componentStore.updateHero(hero);
+    this._service.updateHero(hero);
   }
 }
--- src/app/features/hero-detail/pages/hero-detail/hero-detail.service.ts
+++ src/app/features/hero-detail/pages/hero-detail/hero-detail.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { Hero } from '../../../../domain/hero';
+import { HeroApi } from '../../../../infrastructures/api/hero.api';
 
-@Injectable({
-  providedIn: 'root'
-})
+interface HeroDetailState {
+  hero: Hero | null;
+}
+
+const initialState: HeroDetailState = {
+  hero: null,
+};
+
+@Injectable()
 export class HeroDetailService {
+  private readonly _heroApi = inject(HeroApi);
 
-  constructor() { }
+  readonly $state = signal<HeroDetailState>(initialState);
+
+  async getHero(id: number): Promise<void> {
+    const hero = await firstValueFrom(this._heroApi.getHero(id));
+    this.$state.set({ hero });
+  }
+
+  async updateHero(hero: Hero): Promise<void> {
+    await firstValueFrom(this._heroApi.updateHero(hero));
+    this.$state.set({ hero });
+  }
 }
